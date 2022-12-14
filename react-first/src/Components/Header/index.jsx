68e59df4c7693ae7de0c14a2cd6ed5acd4cffd75
Aram Aslanyan <aslanyan99.99@bk.ru>
2022-12-14 22:12:40 +0400

    React 14-12

---
+++ react-first/src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component } from "react";
+import "./style.css";
+
+class Header extends Component
+{
+	render() {
+		return <div>
+				<p>Hello World</p>
+		</div>
+	}
+}
+
+export default Header
