ea2eeef02367b38fac12c36e685110e2292878c1
Aram Aslanyan <aslanyan99.99@bk.ru>
2022-12-19 21:57:57 +0400

    das 2

--- react-first/src/Components/Header/index.jsx
+++ react-first/src/Components/Header/index.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Component } from "react";
 import "./style.css";
 
-class Header extends Component
-{
+class Header extends Component {
+	constructor(props) {
+		super(props)
+		this.state={ 
+			age:17,
+			weight: '143kg'
+		}
+	}
 	render() {
 		return <div>
-				<p>Hello World</p>
+			<p>Hello World</p>
+			<h2>{this.state.age}</h2>
+			<h2>{this.state.weight}</h2>
 		</div>
 	}
 }
